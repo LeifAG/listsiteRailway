d2c5a909f9a88e6b91038218fa8ec795cde86a97
LeifAG <leif.hammar@gymnasium.ax>
2023-03-07 15:09:35 +0200

    sk

--- listsite/settings.py
+++ listsite/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_-)7p@xe=zb90l@=$dizik!h%-$jmcr=d$=58s6(-6go)a@2zf'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
