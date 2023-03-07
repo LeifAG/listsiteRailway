9e11051a1a9e8c90cf2b79a03d4f4a73e210c172
LeifAG <leif.hammar@gymnasium.ax>
2023-03-07 13:14:27 +0200

    csrfandhosts

--- listsite/settings.py
+++ listsite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = ['web-production-4341.up.railway.app','localhost','127.0.0.1']
-CSRF_TRUSTED_ORIGINS = ['web-production-4341.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://web-production-4341.up.railway.app']
 
 
 # Application definition
